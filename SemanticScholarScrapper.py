248d93eedd18ea46f9909809e04190f20f7794f1
davidAlgis <david.algis@tutamail.com>
2022-09-09 10:40:25 +0200

    🎉 Add main that create and handle a tkinter gui and semanticScholar for scrapping

---
+++ SemanticScholarScrapper.py
@@ -0,0 +1,298 @@
+# code based on https://github.com/dnasc/semantic-scholar-scrapper/blob/master/main.py
+# great re
+import os
+import sys
+import time
+
+import distance
+from collections import deque
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from tkinter import messagebox
+
+
+class SemanticScholarScrapper(object):
+    """
+    A Web Scrapper for Semantic Scholar.
+    """
+    # _web_driver = webdriver.Firefox()
+
+    def __init__(self, log_file, timeout=15, time_between_api_call=0.3, headless=True,
+                 site_url='https://www.semanticscholar.org/',
+                 site_sign_in_url='https://www.semanticscholar.org/sign-in'):
+        """
+
+        :param timeout: Number of seconds the web driver should wait before raising a timeout error.
+        :param time_between_api_call: Time in seconds between api calls.
+        :param headless: If set to be true, the web driver launches a browser (chrome) in silent mode.
+        :param site_url: Home for semantic scholar search engine.
+        """
+        self._site_url = site_url
+        self._site_sign_in_url = site_sign_in_url
+        self._web_driver: webdriver.Firefox = None
+
+        self._timeout = timeout
+        self._time_between_api_call = time_between_api_call
+        self._headless = headless
+        self.is_connected = False
+        self.log_file = log_file
+
+    def scrap_paper_list_by_title(self, paper_title_list: list) -> dict:
+        """
+        Given a list of paper titles, this method retrieves their associated data from semantic scholar.
+
+        :param paper_title_list: A list of paper titles.
+
+        :return: A dictionary of dictionaries containing papers data.
+        """
+
+        self._start_browser()
+        papers_dict = dict()
+
+        for paper_name in tqdm.tqdm(paper_title_list):
+            try:
+                paper_dict = self.scrap_paper_by_title(
+                    paper_name, call_browser=False)
+                paper_id = paper_dict['paperId']
+                papers_dict[paper_id] = paper_dict
+            except KeyError:
+                pass
+
+        self._close_browser()
+
+        return papers_dict
+
+    def scrap_paper_by_title(self, paper_title: str, call_browser=True) -> bool:
+        """
+        Given a paper title, this method retrieves its associated data from semantic scholar.
+
+        :param paper_title: A paper title.
+        :param call_browser: True when web browser hasn't be started yet.
+
+        return:
+        """
+        if call_browser:
+            self._start_browser()
+
+        self._search_paper_by_name(paper_title)
+        hasOpenFirstLink = self._open_first_link_in_search_page()
+        if(hasOpenFirstLink == False):
+            return False
+        return self._check_paper_page(paper_title)
+
+    def _open_first_link_in_search_page(self) -> bool:
+        """
+        Given the browser is on a search page, go to the first paper link.
+        """
+
+        hasFind = self._wait_element_by_class_name(
+            'dropdown-filters__result-count')
+
+        if(hasFind == False):
+            return False
+
+        papers_div = self._web_driver.find_element(By.CLASS_NAME,
+                                                   'result-page')
+        first_paper_link = papers_div.find_element(
+            By.CLASS_NAME, 'cl-paper-title')
+
+        first_paper_link.click()
+        return True
+
+    def _check_paper_page(self, paper_title) -> bool:
+        """
+        Check if opened paper web page is indeed related to paper title.
+
+        :param paper_title: A paper title.
+
+        :return: true if the paper result and search seems to corresponds, else false
+        """
+
+        self._wait_element_by_tag_name('h1')
+
+        h1_list = self._web_driver.find_elements(By.TAG_NAME, 'h1')
+        h1 = [h1 for h1 in h1_list if h1.get_attribute(
+            'data-selenium-selector') == 'paper-detail-title'][0]
+        title = h1.text
+
+        if not 0 <= distance.levenshtein(paper_title, title) <= 10:
+            return False
+        return True
+
+    def _search_paper_by_name(self, paper_title) -> None:
+        """
+        Go to the search page for 'paper_name'.
+        """
+
+        self._web_driver.get(str(
+            "https://www.semanticscholar.org/search?q=" + paper_title + "&sort=relevance"))
+
+    def _wait_element_by_tag_name(self, tag_name, msg="") -> bool:
+        """
+        Make driver wait while web browser loads tags with specific name.
+        """
+
+        try:
+            element_present = expected_conditions.presence_of_element_located(
+                (By.TAG_NAME, tag_name))
+            WebDriverWait(self._web_driver, self._timeout).until(
+                element_present)
+        except TimeoutException:
+            print("Error - " + msg +
+                  "\nTimeoutException - could not find "+class_name)
+            self.log_file.write(
+                "Error - " + msg + "\nTimeoutException - could not find "+class_name)
+            return False
+            # raise
+        return True
+
+    def _wait_element_by_name(self, name, msg="") -> bool:
+        """
+        Make driver wait while browser loads elements with specific name.
+        """
+
+        try:
+            element_present = expected_conditions.presence_of_element_located(
+                (By.NAME, name))
+            WebDriverWait(self._web_driver, self._timeout).until(
+                element_present)
+        except TimeoutException:
+            print("Error - " + msg +
+                  "\nTimeoutException - could not find "+class_name)
+            self.log_file.write(
+                "Error - " + msg + "\nTimeoutException - could not find "+class_name)
+            return False
+            # raise
+        return True
+
+    def _wait_element_by_class_name(self, class_name, msg="") -> bool:
+        """
+        Make driver wait while browser loads elements with specific class name.
+        """
+
+        try:
+            element_present = expected_conditions.presence_of_element_located(
+                (By.CLASS_NAME, class_name))
+            WebDriverWait(self._web_driver, self._timeout).until(
+                element_present)
+        except TimeoutException:
+            print("Error - " + msg +
+                  "\nTimeoutException - could not find "+class_name)
+            self.log_file.write(
+                "Error - " + msg + "\nTimeoutException - could not find "+class_name)
+            return False
+            # raise
+        return True
+
+    def _start_browser(self):
+        self._web_driver = webdriver.Firefox()
+
+    def _close_browser(self):
+        self._web_driver.close()
+
+    def connect_to_account(self, email, passwd) -> bool:
+        """
+        Connect with the given email and password to a SemanticScholar account 
+        wait until it has been connected
+        """
+        self._start_browser()
+        self._web_driver.get(self._site_sign_in_url)
+        id_email = 'email'
+        hasFind = self._wait_element_by_name(
+            id_email, "Could not connect to the login page.")
+
+        if(hasFind == False):
+            return False
+        email_input_box = self._web_driver.find_element("name", id_email)
+        email_input_box.send_keys(email)
+
+        passwd_input_box = self._web_driver.find_element(
+            By.XPATH, "//input[@type='password']")
+        passwd_input_box.send_keys(passwd)
+        login_button_class = "cl-button__label"
+
+        login_button = self._web_driver.find_element(
+            By.XPATH, "//span[text()='Sign In']")
+        login_button.click()
+
+        hasFind = self._wait_element_by_class_name(
+            "search-input__label", "Unable to sign-in.")
+        if(hasFind == False):
+            return False
+
+        self.is_connected = True
+        return True
+
+    def alert(self):
+        """
+        Add an alert on an article. Make sure to be in the page 
+        of the article, before calling this function
+        """
+        disableAlert = False
+        try:
+            self._web_driver.find_element(
+                By.XPATH, "//span[text()='Disable Alert']")
+        except NoSuchElementException:
+            disableAlert = True
+
+        if(disableAlert == False):
+            return True
+
+        alertText = 'Activate Alert'
+        try:
+            self._web_driver.find_element(
+                By.XPATH, str("//span[text()='"+alertText+"']"))
+        except NoSuchElementException:
+            alertText = 'Create Alert'
+            try:
+                self._web_driver.find_element(
+                    By.XPATH, str("//span[text()='"+alertText+"']"))
+            except NoSuchElementException:
+                if(disableAlert):
+                    print("Unable to add alert")
+                    return False
+
+        self._web_driver.find_element(
+            By.XPATH, str("//span[text()='"+alertText+"']")).click()
+        return True
+
+    def save_to_library(self) -> bool:
+        """
+        Save an article to library. Make sure to be in the page 
+        of the article, before calling this function
+        """
+        inLibrary = False
+        try:
+            self._web_driver.find_element(
+                By.XPATH, "//span[text()='In Library']")
+        except NoSuchElementException:
+            inLibrary = True
+
+        if(inLibrary == False):
+            return True
+
+        try:
+            self._web_driver.find_element(
+                By.XPATH, "//span[text()='Save to Library']")
+        except NoSuchElementException:
+            if(inLibrary):
+                print("ne trouve pas le bouton librairie")
+                return False
+
+        self._web_driver.find_element(
+            By.XPATH, "//span[text()='Save to Library']").click()
+
+        return True
+
+
+# s = SemanticScholarScrapper()
+# s.connect_to_account("david.algis@tutamail.com", "mdpTest67")
+# s.scrap_paper_by_title("Simulating Ocean Water", False)
---
+++ main.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import csv
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter import messagebox
+from SemanticScholarScrapper import *
+import os
+
+
+class MainGUI(object):
+
+    def __init__(self):
+
+        self.path, filename = os.path.split(os.path.realpath(__file__))
+        self.root = tk.Tk()
+        self.root.title('Send files to semanticScholar')
+        self.root.geometry('350x250')
+        self.root.protocol("WM_DELETE_WINDOW", self.onClosing)
+        # Email entry:
+        self.lblInfo = ttk.Label(self.root, text='Sign in SemanticScholar:')
+        self.lblEmail = ttk.Label(self.root, text='Email:')
+        self.entryEmail = ttk.Entry(self.root)
+
+        # Password entry:
+        self.lblPasswd = ttk.Label(self.root, text='Password:')
+        self.entryPasswd = ttk.Entry(self.root, show='*')
+
+        self.buttonSelectFiles = ttk.Button(
+            self.root,
+            text='Select a csv file exported by zotero...',
+            command=self._selectFiles
+        )
+
+        self.separator = ttk.Separator(self.root, orient='horizontal')
+
+        self.buttonSendData = ttk.Button(
+            self.root,
+            text='Send data to SemanticScholar.com...',
+            command=self._sendDataToSemanticscholar
+        )
+
+        self.lblLoading = ttk.Label(
+            self.root, text='Waiting for a file to be selected...')
+
+        self.fileName = ""
+        self.data = pd.DataFrame()
+        self.email = ""
+        self.passwd = ""
+        self.hasFile = False
+        self._pack()
+        self.saveFileName = self.path + "//saveDataSC.csv"
+        self.saveFile = None
+        self.logFileName = self.path + "//log.txt"
+        self.saveFileDataFrame = pd.DataFrame()
+        self._initSaveData()
+        self.logFile = open(self.logFileName, "a", encoding="utf-8")
+        self.hasAlreadySaveFile = False
+
+    def _initSaveData(self):
+        if(os.path.exists(self.saveFileName) == False):
+            self.saveFile = open(self.saveFileName, "a", encoding="utf-8")
+            self.saveFile.write("\"Key\",\"Title\"\n")
+        else:
+            self.saveFile = open(self.saveFileName, "a", encoding="utf-8")
+            self.hasAlreadySaveFile = True
+            self.saveFileDataFrame = pd.read_csv(self.saveFileName)
+            # print(self.saveFileDataFrame)
+            # print((self.saveFileDataFrame['Key'] == "AT56HMFX").any())
+
+    def onClosing(self):
+        # if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        self.root.destroy()
+        self.saveFile.close()
+        self.logFile.close()
+
+    def _pack(self):
+        self.lblInfo.pack(anchor='nw')
+        self.lblEmail.pack()
+        self.entryEmail.pack()
+        self.lblPasswd.pack()
+        self.entryPasswd.pack()
+        self.separator.pack(fill='x', pady=10)
+        self.buttonSelectFiles.pack(expand=True,
+                                    fill='both', padx=10, pady=10)
+        self.separator.pack(fill='x')
+        self.buttonSendData.pack(expand=True,
+                                 fill='both', padx=10, pady=10)
+        self.lblLoading.pack(expand=True,
+                             fill='both')
+
+    def _selectFiles(self):
+        filetypes = (
+            ('csv files', '*.csv'),
+            ('All files', '*.*')
+        )
+
+        self.fileName = fd.askopenfilename(
+            title='Open a file',
+            initialdir='C://Users',
+            filetypes=filetypes)
+        self._csvToDataFrame()
+
+    def _csvToDataFrame(self):
+        self.lblLoading.config(text="Reading library...")
+
+        self.data = pd.read_csv(self.fileName)
+
+        val = []
+        for i in range(len(self.data.index)):
+            val.append(True)
+
+        self.data.insert(0, 'Ajouter une alarme', val)
+        self.data.insert(0, 'Ajouter dans la librairie', val)
+
+        self.data = self.data[['Ajouter une alarme', 'Ajouter dans la librairie',
+                               'Key', 'Title', 'Author', 'Item Type', 'Publication Year']]
+        self.data = self.data.loc[self.data['Item Type'] == 'journalArticle']
+        self.hasFile = True
+        # for index, row in self.saveFileDataFrame.iterrows():
+        #     if((self.data['Key'] == row['Key']).any()):
+        #         self.data = self.data.drop(self.data.loc(self.data['Key'] == row['Key']))
+        # print(data)
+
+    def _sendDataToSemanticscholar(self):
+        self.lblLoading.config(text="Connection to semanticScholar.com...")
+
+        self.email = self.entryEmail.get()
+        self.passwd = self.entryPasswd.get()
+        if(self.email == '' or self.passwd == ''):
+            # TODO: to remove
+            self.email = "david.algis@tutamail.com"
+            self.passwd = "mdpTest67"
+            self.lblLoading.config(text="Please sign-in above")
+            #messagebox.showerror('Error', 'Please fill the login field above')
+            # return
+        if(self.hasFile == False):
+            self.fileName = "C://Users//david//Documents//Recherches//pluginZoteroSemanticSchool//these.csv"
+            self.lblLoading.config(text="Please select a file...")
+            self._csvToDataFrame()
+            # TODO: to remove
+            # messagebox.showerror(
+            #     'Error', 'Please select a csv file containing your zotero librairies')
+            # return
+        messagebox.showinfo(
+                'Info', 'The application may not responding during scrapping.\n Go make yourself a coffee, it may take a few minutes.')
+        scrapper = SemanticScholarScrapper(self.logFile)
+        self.lblLoading.config(text="Login in progress...")
+        isConnected = scrapper.connect_to_account(self.email, self.passwd)
+        if(scrapper.is_connected == True):
+            self.lblLoading.config(text="Sending data to semanticScholar...")
+        else:
+            self.lblLoading.config(
+                text="Unable to connect to SemanticScholar !")
+            messagebox.showerror(
+                'Error', 'Unable to connect to SemanticScholar !\nPlease check you login information or you connection and try again.')
+            return
+        Alert = ""
+        for index, row in self.data.iterrows():
+            if((self.saveFileDataFrame['Key'] == row['Key']).any()):
+                print("skip : " + row['Title'])
+                continue
+            print("searching " + row['Title'])
+            hasAddPaper = scrapper.scrap_paper_by_title(
+                row['Title'], False)
+            if(hasAddPaper == False):
+                msg = "Could not add " + row['Title']+"\n"
+                self.writeInLog(msg)
+                Alert += msg
+                continue
+            addAlert = scrapper.alert()
+            saveToLibrary = scrapper.save_to_library()
+            if(addAlert == False and saveToLibrary == False):
+                msg = "Could not add alert on " + row['Title']+"\n"
+                self.writeInLog(
+                    msg)
+                Alert += msg
+                continue
+            if(addAlert == False):
+                self.writeInLog(
+                    "Could not add alert on " + row['Title'] + ", but add it to library.\n")
+            if(saveToLibrary == False):
+                self.writeInLog(
+                    "Could not save " + row['Title'] + "in library, but add it to alert.\n")
+            self.saveFile.write("\""+row['Key']+"\", \""+row['Title']+"\"\n")
+            self.writeInLog("add "+ row['Title'] + " to save file.\n")
+            print(self.saveFileName)
+
+        self.lblLoading.config(text="Finish sending data.")
+
+        if(Alert != ''):
+            messagebox.showerror('Scrapping is over', Alert)
+
+    def writeInLog(self, msg):
+        print(msg)
+        self.logFile.write(msg)
+
+    def MainLoop(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    gui = MainGUI()
+    gui.MainLoop()
