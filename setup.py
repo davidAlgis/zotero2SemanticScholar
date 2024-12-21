bde2b9f46928c38312c57d61437fd19e2ee6cffd
davidAlgis <david.algis@tutamail.com>
2024-12-21 12:33:33 +0100

    ✨ Update setup to create an exe with cx_freeze

--- setup.py
+++ setup.py
@@ -1,16 +1,37 @@
-from setuptools import setup, find_packages
+import sys
+from cx_Freeze import setup, Executable
+from setuptools import find_packages
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+# Load README and requirements
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
 
-requirements = ["pandas>=1.3", "selenium>=4", "Distance>=0.1"]
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
 
+# Dependencies to be included
+build_exe_options = {
+    "packages": [
+        "os", "pandas", "seleniumbase", "undetected_chromedriver", "distance",
+        "Levenshtein"
+    ],
+    "include_files": ["SemanticScholarScrapper.py", "requirements.txt"],
+    "excludes": ["tkinter.test"],
+}
+
+# Add base for Windows GUI applications
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# Main setup configuration
 setup(
     name="Zotero2SemanticScholar",
     version="0.1",
     author="David Algis",
     author_email="david.algis@tutamail.com",
-    description="A package to send zotero libraby to semantic scholar and add alert on them",
+    description=
+    "A package to send Zotero library entries to Semantic Scholar and add alerts for them.",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/davidAlgis/zotero2SemanticScholar",
@@ -19,5 +40,14 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    options={"build_exe": build_exe_options},
+    executables=[
+        Executable(
+            script="main.py",  # Your main Python file
+            base=base,
+            target_name="Zotero2SemanticScholar.exe",  # Name of the executable
+            icon=None,  # Add an icon path here if you have one
+        )
     ],
 )
